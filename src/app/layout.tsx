691d91125d9a74e85fcb63af1c9264027ac895af
paulo <paulofvilarimda@gmail.com>
2024-01-22 02:55:21 -0300

    icon metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,15 +5,17 @@ import { Abel as FontSans } from "next/font/google";
 import { TRPCReactProvider } from "~/trpc/react";
 import Navbar from "~/components/navbar/navbar";
 import Footer from "~/components/footer/footer";
+import { type Metadata } from "next";
 
 const fontSans = FontSans({
   subsets: ["latin"],
   weight: "400",
 });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Struct EJ",
   description: "Struct EJ - Empresa júnior de engenharia de computação",
+  icons: {icon: "/foguete.png"}
 };
 
 export default function RootLayout({
