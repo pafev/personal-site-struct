3c52bd3faa800b18d7646ce4c71f57cf81ad13e8
paulo <paulofvilarimda@gmail.com>
2024-01-23 11:56:49 -0300

    responsive header contactUs

--- src/components/landingPage/contactUs/contactUs.tsx
+++ src/components/landingPage/contactUs/contactUs.tsx
@@ -1,18 +1,18 @@
 import ContactUsForm from "./contactUsForm";
 
-export default function ContactUs() {  
+export default function ContactUs() {
   return (
-    <section className="flex flex-col w-full self-center pt-28" id="contato">
-      <div className="flex flex-col py-6 sm:h-[35rem] w-full items-start justify-center bg-gradient-to-br from-sky-700 via-sky-900 to-sky-900 px-8">
-        <h1 className="lg:w-[40%] text-6xl font-bold mb-4">
+    <section className="flex w-full flex-col self-center pt-28" id="contato">
+      <div className="flex w-full flex-col items-start justify-center bg-gradient-to-br from-sky-700 via-sky-900 to-sky-900 px-8 py-6 md:h-[20rem] lg:h-[35rem]">
+        <h1 className="mb-4 text-6xl font-bold xl:w-[40%]">
           Entre em contato com a gente
         </h1>
-        <p className="w-[30%] text-lg">
+        <p className="px-8 text-lg xl:w-[30%]">
           Explicação de como será útil o cliente entrar em contato e como
           entraremos em retorno
         </p>
       </div>
-      <ContactUsForm/>
+      <ContactUsForm />
     </section>
   );
 }
