835da486375e647dfba68bd641009ec43cdb5527
paulo <paulofvilarimda@gmail.com>
2024-01-23 12:37:52 -0300

    responsive div whatWeDo

--- src/components/landingPage/whatWeDo/whatWeDo.tsx
+++ src/components/landingPage/whatWeDo/whatWeDo.tsx
@@ -7,7 +7,7 @@ export default function WhatWeDo() {
       className="flex w-[90%] items-center justify-around flex-col lg:flex-row self-center pt-40"
       id="o-que-fazemos"
     >
-      <div className="absolute left-0 -z-10 lg:mt-[25rem] h-[20rem] w-[15rem] rounded-e-[30%] bg-gradient-to-br from-sky-600 via-sky-700 to-sky-900"></div>
+      <div className="absolute left-0 -z-10 -mt-[25rem] lg:mt-[25rem] h-[16rem] lg:h-[20rem] w-full lg:w-[15rem] lg:rounded-e-[30%] bg-gradient-to-tr lg:bg-gradient-to-br from-sky-600 via-sky-700 to-sky-900"></div>
       <CloudinaryImg
         src="developer_puhlbm"
         alt="office"
